d4d6243bac79cff4e479b69d2c4ccca04afd139e
104012 <104012@broklede.nl>
2016-03-31 22:11:41 +0200

    final with comments

--- new.py
+++ new.py
@@ -23,6 +23,7 @@ class Derivative:
 
 
 class Integrator:
+    """ RK4 Integrator class """
     def __init__(self, accelerate, w):
         self.accelerate = accelerate
         self.w = w
@@ -58,31 +59,34 @@ class Integrator:
     
 
 
-def Kx(u):
-    data = [
-        [1.962, 0.008],
-        [2.943, 0.019],
-        [3.924, 0.035],
-        [4.905, 0.053],
-        [5.886, 0.067],
-        [6.867, 0.094],
-    ]
-
-    a = 537.48
-    b = 1.6878
+def Kx(u): # F = cu
+    """Force as function of displacement"""
+    a = 140.98
+    b = 1.
     #a = 24.181
     #b = 0.5329
 
     return a * abs(u) ** b
 
 
-def invKx(F):
-    a = 537.48
-    b = 1.6878
+def invKx(F): # u = F/c
+    """Inverse force"""
+    #a = 537.48
+    #b = 1.6878
+    a = 140.98
+    b = 1.
     return abs(F/a) ** (1/b)
 
 
 def Accelerator(u0, m, R, r, k):
+    """
+    Generate a new instance of our 'motor'
+    
+    Should have actually accounted for the fact that the axle radius
+    increases when the u increases, because the rubber band wraps around
+    itself making the radius larger. Maybe even more accuracy.
+        
+    """
     
     def A(state):
         x = state.x
@@ -96,13 +100,11 @@ def Accelerator(u0, m, R, r, k):
 
         Fe = Kx(u)
 
+        # account for slip
         Fw = min(r/R * Fe, mu*m*g)
-        #Fw = r/R * Fe
-        Fd = k * v**2
-        Frr = Crr * m * g
-        #Fb = 0.5 * 0.001 * 0.0015 * m*g / r
-        #Fb = 2 * 0.0015 * m * g * 0.0065 / R
-        Fb = 0.068
+        Fd = k * v**2 # drag
+        Frr = Crr * m * g # rolling resistance
+        Fb = 0.07 # hardcode bearing resistance, i.e., internal friction
 
 
         Ft = Fw - Fd - Frr - Fb
@@ -113,45 +115,68 @@ def Accelerator(u0, m, R, r, k):
     return A
 
 
-dt = 0.05
+dt = 0.05 # Smaller doesn't add significant accuracy, unlike Euler's method.
 g = 9.81
-tmax = 20
+tmax = 20 # 20 seconds
 
-mu = 0.7
-Crr = 0.015
+mu = 0.6 # grip coefficient
+Crr = 0.015 # rolling resistance coefficient
 
 
 def drag(n):
-    rho = 1.225
+    """ Calculate drag as a function of # of CDs as wheels
+            CD has mass of 15 g
+            Radius 12 cm
+            Thickness 1.5 mm
+            Assume drag coefficient 0.5 (of a cylinder)
+
+            Car frontal area estimate 6 cm X 6 cm, assume it's a block.
+    """
+
+    rho = 1.225 # air density
     wheel = 0.5 * rho * (n * 0.0015 * 0.12) / 2.0
     body  = 1.05 * rho * (0.06 * 0.06) / 2.0
     return 2 * wheel + body
 
 
-def run(m, R, r, k):
+def run(m, R, r, k, u0=0):
+    """
+    Run simulation. Input:
+        m: Mass of car in kg
+        R: Radius of back wheels
+        r: Radius of axle
+        k: Air resistance constant (calculated by the drag function)
+        u0: Starting position rubber band, in cm
+
+    """
+
     trange = np.arange(0, tmax, dt)
     df = pd.DataFrame(columns=["x", "v", "a", "u"], index=trange)
 
-    u0 = invKx(R/r * mu * m*g)
-    if u0 > 0.20:
-        u0 = 0.20
-    #u0 = 0.140
+    if not u0: 
+        u0 = invKx(R/r * mu * m*g) # determine max u before car slipping
+        if u0 > 0.20: # set max uitrekking to 20 cm
+            u0 = 0.20
 
-    a = Accelerator(u0, m, R, r, k)
-    Int = Integrator(a, r/R)
+    a = Accelerator(u0, m, R, r, k) # our motor, based on given parameters
+    Int = Integrator(a, r/R) 
 
     state = State(0, 0, -u0)
     for t in trange:
-        df.ix[t] = [state.x, state.v, state.a, state.u]
+        df.ix[t] = [state.x, state.v, state.a, state.u] # log current state
         if state.v < 0:
             break
-        state = Int.integrate(state, t, dt)
+        state = Int.integrate(state, t, dt) # get next state
 
-    df.u[df.u > 0] = 0
+    df.u[df.u > 0] = 0 # filter unnecessary results
     return df
 
 
 def optimize_cd():
+    """ Find optimal number of CD's needed as wheels, for fastest time
+        More CDs mean better grip, but also more mass, so find optimal balance.
+    """
+
     cds = np.arange(2, 35)
     
     # optimal time
@@ -165,8 +190,8 @@ def optimize_cd():
     srmax = 0
 
     for n in cds:
-        m = 2 * n * 0.015 + 0.1
-        x = run(m, 0.06, 0.008, drag(n))
+        m = 2 * n * 0.015 + 0.14
+        x = run(m, 0.06, 0.006, drag(n))
         t = x.index[x.x >= 8].min()
         s = x.x.max()
         
@@ -188,8 +213,18 @@ def optimize_cd():
     
 
 def optimize():
-    mass = np.arange(0.4, 1.4, 0.02)
-    radi = np.arange(0.8/100., 1.4/100., 0.1/100.)
+    """
+    Find the best mass vs axle radius.
+        Larger axle -> greater acceleration for shorter period
+        Smaller axle -> smaller acceleration for longer period
+
+        More mass -> smaller acceleration
+        Less mass -> greater acceleration
+    What is optimal?
+    """
+
+    mass = np.arange(0.1, 0.5, 0.02) # 100 g to 500 g, 20 g steps
+    radi = np.arange(0.3/100., 1.2/100., 0.05/100.) # axle radius, 0.3 cm to 1.2 cm, .5 mm steps
 
     # optimal time
     tmin = 1000
@@ -204,7 +239,7 @@ def optimize():
 
     for m in mass:
         for r in radi:
-            x = run(m, 0.06, r, drag(2))
+            x = run(m, 0.06, r, drag(4))
             t = x.index[x.x >= 8].min()
             s = x.x.max()
             
@@ -226,19 +261,22 @@ def optimize():
     
 
 def main():
+    """ Command line parser, easy running """
     parser = argparse.ArgumentParser()
     parser.add_argument("-O", "--optimize", action="store_true")
     args = parser.parse_args()
 
     if args.optimize:
-        o = optimize_cd()
+        o = optimize()
         print o
-        x = run(o[0], 0.06, 0.008, drag(o[1]))
+        #x = run(o[0], 0.06, 0.008, drag(o[1]))
     else:
-        x = run(0.80, 0.06, 0.008, drag(20))
+        x = run(0.24, 0.06, 0.004, drag(2))
+        x = x.dropna()
         print x
 
     x.plot()
+    plt.savefig("race.png")
     plt.show()
     
         
